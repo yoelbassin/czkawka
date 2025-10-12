e47d11d783411264db1a193101a3b76b629660b0
Rafał Mikrut <mikrutrafal@protonmail.com>
2025-10-12 15:34:41 +0200

    A

--- czkawka_gui/src/create_tree_view.rs
+++ czkawka_gui/src/create_tree_view.rs
@@ -1,6 +1,6 @@
 use gtk4::prelude::*;
 use gtk4::{CellRendererText, CellRendererToggle, ListStore, TreeView, TreeViewColumn};
-use crate::gui_structs::common_tree_view::{create_default_column, create_default_selection_button_column};
+use crate::gui_structs::common_tree_view::{create_default_columns, create_default_selection_button_column, ColumnSort};
 use crate::help_functions::{
     ColumnsBadExtensions, ColumnsBigFiles, ColumnsBrokenFiles, ColumnsDuplicates, ColumnsEmptyFiles, ColumnsEmptyFolders, ColumnsExcludedDirectory, ColumnsIncludedDirectory,
     ColumnsInvalidSymlinks, ColumnsSameMusic, ColumnsSimilarImages, ColumnsSimilarVideos, ColumnsTemporaryFiles, get_list_store,
@@ -11,11 +11,24 @@ use crate::help_functions::{
 pub(crate) fn create_tree_view_included_directories(tree_view: &TreeView) {
     let model = get_list_store(tree_view);
 
-    create_default_column(tree_view, ColumnsIncludedDirectory::Path as i32, Some(None), None);
+    create_default_columns(
+        tree_view,
+        &[
+            (ColumnsIncludedDirectory::Path as i32, ColumnSort::Default),
+        ],
+        None,
+    );
+
     create_default_selection_button_column(tree_view, ColumnsIncludedDirectory::ReferenceButton as i32, model, None);
 }
 
 pub(crate) fn create_tree_view_excluded_directories(tree_view: &TreeView) {
     tree_view.set_headers_visible(false);
-    create_default_column(tree_view, ColumnsExcludedDirectory::Path as i32, Some(None), None);
+    create_default_columns(
+        tree_view,
+        &[
+            (ColumnsExcludedDirectory::Path as i32, ColumnSort::Default),
+        ],
+        None,
+    );
 }
--- czkawka_gui/src/gui_structs/common_tree_view.rs
+++ czkawka_gui/src/gui_structs/common_tree_view.rs
@@ -7,7 +7,6 @@ use crate::notebook_info::{NOTEBOOKS_INFO, NotebookObject};
 use glib::translate::ToGlibPtr;
 use gtk4::prelude::*;
 use gtk4::{Builder, CellRendererText, CellRendererToggle, EventControllerKey, GestureClick, ListStore, Notebook, ScrolledWindow, TreeView, TreeViewColumn};
-use gtk4::{CellRendererText, TreeViewColumn};
 
 #[derive(Clone)]
 pub struct CommonTreeViews {
@@ -60,15 +59,18 @@ pub struct SubView {
     pub event_controller_key: EventControllerKey,
     pub notebook_object: NotebookObject,
     pub enum_value: NotebookMainEnum,
+    pub preview: Option<gtk4::Picture>
 }
 
 impl SubView {
-    pub fn new(builder: &Builder, scrolled_name: &str, enum_value: NotebookMainEnum) -> Self {
+    pub fn new(builder: &Builder, scrolled_name: &str, enum_value: NotebookMainEnum, preview: Option<&str>) -> Self {
         let tree_view: TreeView = TreeView::new();
         let event_controller_key: EventControllerKey = EventControllerKey::new();
         tree_view.add_controller(event_controller_key.clone());
         let gesture_click: GestureClick = GestureClick::new();
         tree_view.add_controller(gesture_click.clone());
+
+        let preview = preview.map(|name| builder.object(name).expect(format!("Cannot find preview image {}", name).as_str()));
 
         let notebook_object = NOTEBOOKS_INFO[enum_value as usize].clone();
 
@@ -79,118 +81,134 @@ impl SubView {
             event_controller_key,
             notebook_object,
             enum_value,
+            preview
         }
     }
 
     fn setup(&self) {
         let tree_view = &self.tree_view;
         let model = self.tree_view.get_model();
-        self.tree_view.set_vexpand(true);
+        tree_view.set_vexpand(true);
         match self.enum_value {
             NotebookMainEnum::Duplicate => {
                 let columns_colors = (ColumnsDuplicates::Color as i32, ColumnsDuplicates::TextColor as i32);
                 let activatable_colors = (ColumnsDuplicates::ActivatableSelectButton as i32, ColumnsDuplicates::Color as i32);
-
                 create_default_selection_button_column(tree_view, ColumnsDuplicates::SelectionButton as i32, model, Some(activatable_colors));
-
-                create_default_column(tree_view, ColumnsDuplicates::Size as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsDuplicates::Name as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsDuplicates::Path as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsDuplicates::Modification as i32, None, Some(columns_colors));
+                create_default_columns(
+                    tree_view,
+                    &[
+                        (ColumnsDuplicates::Size as i32, ColumnSort::Default),
+                        (ColumnsDuplicates::Name as i32, ColumnSort::Default),
+                        (ColumnsDuplicates::Path as i32, ColumnSort::Default),
+                        (ColumnsDuplicates::Modification as i32, ColumnSort::Default),
+                    ],
+                    Some(columns_colors),
+                );
             }
             NotebookMainEnum::EmptyDirectories => {
                 create_default_selection_button_column(tree_view, ColumnsEmptyFolders::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsEmptyFolders::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsEmptyFolders::Path as i32, Some(None), None);
-                create_default_column(
+                create_default_columns(
                     tree_view,
-                    ColumnsEmptyFolders::Modification as i32,
-                    Some(Some(ColumnsEmptyFolders::ModificationAsSecs as i32)),
+                    &[
+                        (ColumnsEmptyFolders::Name as i32, ColumnSort::Default),
+                        (ColumnsEmptyFolders::Path as i32, ColumnSort::Default),
+                        (ColumnsEmptyFolders::Modification as i32, ColumnSort::Custom(ColumnsEmptyFolders::ModificationAsSecs as i32)),
+                    ],
                     None,
                 );
             }
             NotebookMainEnum::BigFiles => {
                 create_default_selection_button_column(tree_view, ColumnsBigFiles::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsBigFiles::Size as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBigFiles::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBigFiles::Path as i32, Some(None), None);
-                create_default_column(
+                create_default_columns(
                     tree_view,
-                    ColumnsBigFiles::Modification as i32,
-                    Some(Some(ColumnsBigFiles::ModificationAsSecs as i32)),
+                    &[
+                        (ColumnsBigFiles::Size as i32, ColumnSort::Default),
+                        (ColumnsBigFiles::Name as i32, ColumnSort::Default),
+                        (ColumnsBigFiles::Path as i32, ColumnSort::Default),
+                        (ColumnsBigFiles::Modification as i32, ColumnSort::Custom(ColumnsBigFiles::ModificationAsSecs as i32)),
+                    ],
                     None,
                 );
             }
             NotebookMainEnum::EmptyFiles => {
                 create_default_selection_button_column(tree_view, ColumnsEmptyFiles::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsEmptyFiles::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsEmptyFiles::Path as i32, Some(None), None);
-                create_default_column(
+                create_default_columns(
                     tree_view,
-                    ColumnsEmptyFiles::Modification as i32,
-                    Some(Some(ColumnsEmptyFiles::ModificationAsSecs as i32)),
+                    &[
+                        (ColumnsEmptyFiles::Name as i32, ColumnSort::Default),
+                        (ColumnsEmptyFiles::Path as i32, ColumnSort::Default),
+                        (ColumnsEmptyFiles::Modification as i32, ColumnSort::Custom(ColumnsEmptyFiles::ModificationAsSecs as i32)),
+                    ],
                     None,
                 );
             }
             NotebookMainEnum::Temporary => {
                 create_default_selection_button_column(tree_view, ColumnsTemporaryFiles::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsTemporaryFiles::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsTemporaryFiles::Path as i32, Some(None), None);
-                create_default_column(
+                create_default_columns(
                     tree_view,
-                    ColumnsTemporaryFiles::Modification as i32,
-                    Some(Some(ColumnsTemporaryFiles::ModificationAsSecs as i32)),
+                    &[
+                        (ColumnsTemporaryFiles::Name as i32, ColumnSort::Default),
+                        (ColumnsTemporaryFiles::Path as i32, ColumnSort::Default),
+                        (ColumnsTemporaryFiles::Modification as i32, ColumnSort::Custom(ColumnsTemporaryFiles::ModificationAsSecs as i32)),
+                    ],
                     None,
                 );
             }
             NotebookMainEnum::SimilarImages => {
                 let columns_colors = (ColumnsSimilarImages::Color as i32, ColumnsSimilarImages::TextColor as i32);
                 let activatable_colors = (ColumnsSimilarImages::ActivatableSelectButton as i32, ColumnsSimilarImages::Color as i32);
-
                 create_default_selection_button_column(tree_view, ColumnsSimilarImages::SelectionButton as i32, model, Some(activatable_colors));
-
-                create_default_column(tree_view, ColumnsSimilarImages::Similarity as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarImages::Size as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarImages::Dimensions as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarImages::Name as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarImages::Path as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarImages::Modification as i32, None, Some(columns_colors));
+                create_default_columns(
+                    tree_view,
+                    &[
+                        (ColumnsSimilarImages::Similarity as i32, ColumnSort::Default),
+                        (ColumnsSimilarImages::Size as i32, ColumnSort::Default),
+                        (ColumnsSimilarImages::Dimensions as i32, ColumnSort::Default),
+                        (ColumnsSimilarImages::Name as i32, ColumnSort::Default),
+                        (ColumnsSimilarImages::Path as i32, ColumnSort::Default),
+                        (ColumnsSimilarImages::Modification as i32, ColumnSort::Default),
+                    ],
+                    Some(columns_colors),
+                );
             }
             NotebookMainEnum::SimilarVideos => {
                 let columns_colors = (ColumnsSimilarVideos::Color as i32, ColumnsSimilarVideos::TextColor as i32);
                 let activatable_colors = (ColumnsSimilarVideos::ActivatableSelectButton as i32, ColumnsSimilarVideos::Color as i32);
-
                 create_default_selection_button_column(tree_view, ColumnsSimilarVideos::SelectionButton as i32, model, Some(activatable_colors));
-
-                create_default_column(tree_view, ColumnsSimilarVideos::Size as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarVideos::Name as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarVideos::Path as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSimilarVideos::Modification as i32, None, Some(columns_colors));
+                create_default_columns(
+                    tree_view,
+                    &[
+                        (ColumnsSimilarVideos::Size as i32, ColumnSort::Default),
+                        (ColumnsSimilarVideos::Name as i32, ColumnSort::Default),
+                        (ColumnsSimilarVideos::Path as i32, ColumnSort::Default),
+                        (ColumnsSimilarVideos::Modification as i32, ColumnSort::Default),
+                    ],
+                    Some(columns_colors),
+                );
             }
             NotebookMainEnum::SameMusic => {
                 let columns_colors = (ColumnsSameMusic::Color as i32, ColumnsSameMusic::TextColor as i32);
                 let activatable_colors = (ColumnsSameMusic::ActivatableSelectButton as i32, ColumnsSameMusic::Color as i32);
-
                 create_default_selection_button_column(tree_view, ColumnsSameMusic::SelectionButton as i32, model, Some(activatable_colors));
-
-                create_default_column(tree_view, ColumnsSameMusic::Size as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Name as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Title as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Artist as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Year as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Bitrate as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Length as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Genre as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Path as i32, None, Some(columns_colors));
-                create_default_column(tree_view, ColumnsSameMusic::Modification as i32, None, Some(columns_colors));
+                create_default_columns(
+                    tree_view,
+                    &[
+                        (ColumnsSameMusic::Size as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Name as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Title as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Artist as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Year as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Bitrate as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Length as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Genre as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Path as i32, ColumnSort::Default),
+                        (ColumnsSameMusic::Modification as i32, ColumnSort::Default),
+                    ],
+                    Some(columns_colors),
+                );
             }
             NotebookMainEnum::Symlinks => {
                 create_default_selection_button_column(tree_view, ColumnsInvalidSymlinks::SelectionButton as i32, model, None);
-
                 create_default_columns(
                     tree_view,
                     &[
@@ -205,24 +223,29 @@ impl SubView {
             }
             NotebookMainEnum::BrokenFiles => {
                 create_default_selection_button_column(tree_view, ColumnsBrokenFiles::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsBrokenFiles::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBrokenFiles::Path as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBrokenFiles::ErrorType as i32, Some(None), None);
-                create_default_column(
+                create_default_columns(
                     tree_view,
-                    ColumnsBrokenFiles::Modification as i32,
-                    Some(Some(ColumnsBrokenFiles::ModificationAsSecs as i32)),
+                    &[
+                        (ColumnsBrokenFiles::Name as i32, ColumnSort::Default),
+                        (ColumnsBrokenFiles::Path as i32, ColumnSort::Default),
+                        (ColumnsBrokenFiles::ErrorType as i32, ColumnSort::Default),
+                        (ColumnsBrokenFiles::Modification as i32, ColumnSort::Custom(ColumnsBrokenFiles::ModificationAsSecs as i32)),
+                    ],
                     None,
                 );
             }
             NotebookMainEnum::BadExtensions => {
                 create_default_selection_button_column(tree_view, ColumnsBadExtensions::SelectionButton as i32, model, None);
-
-                create_default_column(tree_view, ColumnsBadExtensions::Name as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBadExtensions::Path as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBadExtensions::CurrentExtension as i32, Some(None), None);
-                create_default_column(tree_view, ColumnsBadExtensions::ValidExtensions as i32, Some(None), None);
+                create_default_columns(
+                    tree_view,
+                    &[
+                        (ColumnsBadExtensions::Name as i32, ColumnSort::Default),
+                        (ColumnsBadExtensions::Path as i32, ColumnSort::Default),
+                        (ColumnsBadExtensions::CurrentExtension as i32, ColumnSort::Default),
+                        (ColumnsBadExtensions::ValidExtensions as i32, ColumnSort::Default),
+                    ],
+                    None,
+                );
             }
         }
     }
@@ -264,7 +287,7 @@ pub(crate) fn create_default_selection_button_column(
 
 
 
-fn create_default_columns(
+pub(crate) fn create_default_columns(
     tree_view: &TreeView,
     columns: &[(i32, ColumnSort)],
     colors_columns_id: Option<(i32, i32)>,
@@ -287,32 +310,4 @@ fn create_default_columns(
         }
         tree_view.append_column(&column);
     }
-}
-
-#[expect(clippy::option_option)]
-pub(crate) fn create_default_column(
-    tree_view: &TreeView,
-    column_id: i32,
-    sort_column_id: Option<Option<i32>>,
-    colors_columns_id: Option<(i32, i32)>,
-) -> (CellRendererText, TreeViewColumn) {
-    let renderer = CellRendererText::new();
-    let column: TreeViewColumn = TreeViewColumn::new();
-    column.pack_start(&renderer, true);
-    column.set_resizable(true);
-    column.set_min_width(50);
-    column.add_attribute(&renderer, "text", column_id);
-    if let Some(sort_column_id) = sort_column_id {
-        if let Some(sort_column_id) = sort_column_id {
-            column.set_sort_column_id(sort_column_id);
-        } else {
-            column.set_sort_column_id(column_id);
-        }
-    }
-    if let Some(colors_columns_id) = colors_columns_id {
-        column.add_attribute(&renderer, "background", colors_columns_id.0);
-        column.add_attribute(&renderer, "foreground", colors_columns_id.1);
-    }
-    tree_view.append_column(&column);
-    (renderer, column)
 }
